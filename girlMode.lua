dd4fa2a90add07d99606bc7dc585480ed9afb0d6
Dorian Burton <120594826+dburton95@users.noreply.github.com>
2026-08-07 14:54:42 -0400

    Added Girl Mode framework

---
+++ girlMode.lua
@@ -0,0 +1,9 @@
+local girlMode = {}
+
+function girlMode.init(mod)
+
+  mod.content.text:override("_RedsHouse1FMomWakeUpText", "MOM: Right.\nAll girls leave\vhome some day.\vIt said so on TV.\fPROF.OAK, next\ndoor, is looking\vfor you.")
+
+end
+
+return girlMode
--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 return function(mod)
   local kris = require("mods.crystal.kris")
+  local girlMode = require("mods.crystal.girlMode")
 
   kris.init(mod)
+  girlMode.init(mod)
 
 end
