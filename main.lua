f02a9f8916b7c978497c7b3f216ddfb9d1857777
Dorian Burton <120594826+dburton95@users.noreply.github.com>
2026-08-10 14:49:50 -0400

    added credits and sygna zoom image

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
 return function(mod)
   local kris = require("mods.crystal.kris")
   local girlMode = require("mods.crystal.girlMode")
+  local credits = require("mods.crystal.credits")
 
   kris.init(mod)
   girlMode.init(mod)
+  credits.init(mod)
 
 end
